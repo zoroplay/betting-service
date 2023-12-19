797ec76ae9e43b735535f1bbe89b66ac7fb0a37b
Isoboye Ibanibo <elsopi02@yahoo.com>
2023-12-19 17:46:12 +0100

    removed logs

--- src/bets/bets.service.ts
+++ src/bets/bets.service.ts
@@ -371,7 +371,7 @@ export class BetsService {
             return {status: 400, message: "Insufficient balance ", success: false};
 
         let userSelection =  bet.selections
-        console.log("userSelection | "+JSON.stringify(userSelection))
+        // console.log("userSelection | "+JSON.stringify(userSelection))
 
         if(clientSettings == undefined || clientSettings.id == undefined || clientSettings.id == 0 ) {
             // return {status: 400, data: "invalid client"};
