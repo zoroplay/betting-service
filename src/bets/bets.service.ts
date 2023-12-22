4740a1352a198b1bfd681e3dfa8fdf3e0bfa22f3
Isoboye Ibanibo <elsopi02@yahoo.com>
2023-12-22 08:38:19 +0100

    minor fix

--- src/bets/bets.service.ts
+++ src/bets/bets.service.ts
@@ -333,7 +333,7 @@ export class BetsService {
         console.log(betslipId)
         let bet = await this.betRepository
             .createQueryBuilder('bet')
-            .select('bet.id,bet.user_id,bet.username,bet.betslip_id,bet.stake,bet.currency,bet.bet_type,bet.bet_category,bet.total_odd,bet.possible_win,bet.source,bet.total_bets,betwon,bet.status,bet.created,winning.winning_after_tax')
+            .select('bet.id,bet.user_id,bet.username,bet.betslip_id,bet.stake,bet.currency,bet.bet_type,bet.bet_category,bet.total_odd,bet.possible_win,bet.source,bet.total_bets,bet.won,bet.status,bet.created,winning.winning_after_tax')
             .leftJoin(Winning, 'winning', 'bet.id = winning.bet_id')
             .where("bet.betslip_id = :betslipId", {betslipId})
             .andWhere("bet.client_id = :clientId", {clientId})
